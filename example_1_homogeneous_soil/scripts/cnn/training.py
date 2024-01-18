9bb439071f49f4ad869092b706c5d5929ed6ace2
Shashwat <shashwatmjn@gmail.com>
2024-01-18 16:37:58 -0500

    Changed variables from camel case to snake case

--- example_1_homogeneous_soil/scripts/cnn/training.py
+++ example_1_homogeneous_soil/scripts/cnn/training.py
@@ -16,71 +16,74 @@ plt.rcParams['mathtext.fontset'] = 'dejavuserif'
 FONT_SIZE = 12
 
 # Get displacement and force data
-def get_data(fileDirectory):
+def get_data(file_directory):
 
     # Get the training displacement data
-    displacementTrainingData = loadmat(fileDirectory + '/displacementTrainingData.mat')
-    displacementTrainingData = np.array(tuple(displacementTrainingData['data']))
+    displacement_training_data = loadmat(file_directory + '/displacement_training_data.mat')
+    displacement_training_data = np.array(tuple(displacement_training_data['data']))
     
     # Get the training force data
-    forceTrainingData = loadmat(fileDirectory + '/forceTrainingData.mat')
-    forceTrainingData = np.array(tuple(forceTrainingData['data']))
+    force_training_data = loadmat(file_directory + '/force_training_data.mat')
+    force_training_data = np.array(tuple(force_training_data['data']))
     
     # Get the validation displacement data
-    displacementValidationData = loadmat(fileDirectory + '/displacementValidationData.mat')
-    displacementValidationData = np.array(tuple(displacementValidationData['data']))
+    displacement_validation_data = loadmat(file_directory + '/displacement_validation_data.mat')
+    displacement_validation_data = np.array(tuple(displacement_validation_data['data']))
     
     # Get the validation force data
-    forceValidationData = loadmat(fileDirectory + '/forceValidationData.mat')
-    forceValidationData = np.array(tuple(forceValidationData['data']))
+    force_validation_data = loadmat(file_directory + '/force_validation_data.mat')
+    force_validation_data = np.array(tuple(force_validation_data['data']))
     
     # Get the test displacement data
-    displacementTestData = loadmat(fileDirectory + '/displacementTestData.mat')
-    displacementTestData = np.array(tuple(displacementTestData['data']))
+    displacement_test_data = loadmat(file_directory + '/displacement_test_data.mat')
+    displacement_test_data = np.array(tuple(displacement_test_data['data']))
     
     # Get the test force data
-    forceTestData = loadmat(fileDirectory + '/forceTestData.mat')
-    forceTestData = np.array(tuple(forceTestData['data']))
+    force_test_data = loadmat(file_directory + '/force_test_data.mat')
+    force_test_data = np.array(tuple(force_test_data['data']))
 
-    return displacementTrainingData, forceTrainingData, displacementValidationData, forceValidationData, displacementTestData, forceTestData
+    return displacement_training_data, force_training_data, displacement_validation_data, force_validation_data, displacement_test_data, force_test_data
 
-# Define function to normalize the input and output dataset
-def normalize_data(dataUsedToNormalize, dataToBeNnormalized, typeOfDataSubset):
-
-    dataMean = np.mean(dataUsedToNormalize)
-    dataRange = np.max(dataUsedToNormalize) - np.min(dataUsedToNormalize)
-
-    normalizedData = (dataToBeNnormalized - dataMean) / dataRange
-    normalizingParameters = np.array(list([dataMean, dataRange]))
-
-    print('Normalized %s dataset.' %(typeOfDataSubset))
-
-    return normalizedData, normalizingParameters
 
 # Define function to normalize the input and output dataset
-def renormalize_data(dataUsedToRenormalize, dataToBeRennormalized, typeOfDataSubset):
+def normalize_data(data_used_to_normalize, data_to_be_normalized, type_of_data_subset):
 
-    dataMean = np.mean(dataUsedToRenormalize)
-    dataRange = np.max(dataUsedToRenormalize) - np.min(dataUsedToRenormalize)
+    data_mean = np.mean(data_used_to_normalize)
+    data_range = np.max(data_used_to_normalize) - np.min(data_used_to_normalize)
 
-    renormalizedData = (dataToBeRennormalized * dataRange) + dataMean
+    normalized_data = (data_to_be_normalized - data_mean) / data_range
+    normalizing_parameters = np.array(list([data_mean, data_range]))
 
-    print('Renormalized %s dataset.' %(typeOfDataSubset))
+    print('Normalized %s dataset.' %(type_of_data_subset))
 
-    return renormalizedData
+    return normalized_data, normalizing_parameters
 
-class cnn():
 
-    def __init__(self, inputShape):
+# Define function to normalize the input and output dataset
+def renormalize_data(data_used_to_renormalize, data_to_be_rennormalized, type_of_data_subset):
+
+    data_mean = np.mean(data_used_to_renormalize)
+    data_range = np.max(data_used_to_renormalize) - np.min(data_used_to_renormalize)
+
+    renormalized_data = (data_to_be_rennormalized * data_range) + data_mean
+
+    print('Renormalized %s dataset.' %(type_of_data_subset))
+
+    return renormalized_data
+
+
+class CNN():
+
+    def __init__(self, input_shape):
         
-        self.inputShape = inputShape
+        self.input_shape = input_shape
     
     def build_model(self):
 
         model = tf.keras.Sequential([
             
             # Input Layer
-            layers.Input(shape=self.inputShape),
+            layers.Input(shape=self.input_shape),
             
             # Hidden Layers
             layers.Conv1D(filters=480, kernel_size=80, padding='same', kernel_initializer='he_uniform', activation='LeakyReLU'),
@@ -93,12 +96,13 @@ class cnn():
         
         return model
 
+
 class plots:
     
-    def __init__(self, history, fileDirectory):
+    def __init__(self, history, file_directory):
 
         self.history = history
-        self.fileDirectory = fileDirectory
+        self.file_directory = file_directory
         self.fontsize = 15
 
     def loss(self):
@@ -107,19 +111,19 @@ class plots:
 
         # Training and Validation
         loss = self.history.history[loss_name]
-        valLoss = self.history.history['val_' + loss_name]
+        val_loss = self.history.history['val_' + loss_name]
 
-        lossPlot = plt.figure()
+        loss_plot = plt.figure()
         epochs = range(1, len(loss)+1)
         plt.plot(epochs, loss, 'bo--', label = 'Training Loss', markersize = 2)
-        plt.plot(epochs, valLoss, 'go--', label = 'Validation Loss', markersize = 2)
+        plt.plot(epochs, val_loss, 'go--', label = 'Validation Loss', markersize = 2)
         plt.title('Training and Validation Loss', fontsize=self.fontsize)
         plt.xlabel('Epochs', fontsize=self.fontsize)
         plt.ylabel('Loss', fontsize=self.fontsize)
         plt.legend(['Training Loss', 'Validation Loss'], fontsize=self.fontsize)
-        plt.savefig(self.fileDirectory + '/loss.pdf', bbox_inches='tight')
+        plt.savefig(self.file_directory + '/loss.pdf', bbox_inches='tight')
 
-        return lossPlot
+        return loss_plot
 
     def evaluation_metric(self):
 
@@ -127,31 +131,32 @@ class plots:
         
         # Training and Validation
         metric = self.history.history[metric_name]
-        valMetric = self.history.history['val_' + metric_name]
+        val_metric = self.history.history['val_' + metric_name]
 
         metricPlot = plt.figure()
         epochs = range(1, len(metric)+1)
         plt.plot(epochs, metric, 'bo--', label = 'Training Metric', markersize = 2)
-        plt.plot(epochs, valMetric, 'go--', label = 'Validation Metric', markersize = 2)
+        plt.plot(epochs, val_metric, 'go--', label = 'Validation Metric', markersize = 2)
         plt.title('Training and Validation Evaluation Metric', fontsize=self.fontsize)
         plt.xlabel('Epochs', fontsize=self.fontsize)
         plt.ylabel('Evaluation Metric', fontsize=self.fontsize)
         plt.legend(['Training Metric', 'Validation Metric'], fontsize=self.fontsize)
-        plt.savefig(self.fileDirectory + '/evaluation_metric.pdf', bbox_inches='tight')
+        plt.savefig(self.file_directory + '/evaluation_metric.pdf', bbox_inches='tight')
 
         return metricPlot
 
+
 def swap_sensor_timestep(data):
 
-    newData = np.zeros((data.shape[0], data.shape[2], data.shape[1]))
+    new_data = np.zeros((data.shape[0], data.shape[2], data.shape[1]))
 
-    for nSamples in range(newData.shape[0]):
-        for nSensors in range(newData.shape[1]):
-            for nTimesteps in range(newData.shape[2]):
+    for nSamples in range(new_data.shape[0]):
+        for nSensors in range(new_data.shape[1]):
+            for nTimesteps in range(new_data.shape[2]):
 
-                newData[nSamples, nSensors, nTimesteps] = data[nSamples, nTimesteps, nSensors]
+                new_data[nSamples, nSensors, nTimesteps] = data[nSamples, nTimesteps, nSensors]
     
-    return newData
+    return new_data
 
 # Define function to save the dataset
 def save_data(data, fileName, file_directory_to_save_data):
@@ -160,47 +165,49 @@ def save_data(data, fileName, file_directory_to_save_data):
         
     print('Saved ' + fileName)
 
+
 def main():
 
     # Define file structure heirarchy
-    mainFileDirectory = 'YOUR_FILE_DIRECTORY'
-    dividedDataFileDirectory = mainFileDirectory + '/dividedDataset'
-    fileDirectoryToSaveResults = mainFileDirectory + '/cnn/trainingResults'
+    main_file_directory = 'YOUR_FILE_DIRECTORY'
+    divided_data_file_directory = main_file_directory + '/dividedDataset'
+    file_directory_to_save_results = main_file_directory + '/cnn/trainingResults'
 
     # Get displacement and void data
-    displacementTrainingData, forceTrainingData, displacementValidationData, forceValidationData, displacementTestData, forceTestData = get_data(dividedDataFileDirectory)
+    displacement_training_data, force_training_data, displacement_validation_data, force_validation_data, displacement_test_data, force_test_data = get_data(divided_data_file_directory)
 
     # Get the number of timesteps from the initial data
-    with open(dividedDataFileDirectory + '/metadataInitialData.pkl', 'rb') as handle:
-        metaData = pickle.load(handle)
+    with open(divided_data_file_directory + '/metadataInitialData.pkl', 'rb') as handle:
+
+        meta_data = pickle.load(handle)
     
     # Normalize the displacement training datasets
-    displacementValidationData = normalize_data(dataUsedToNormalize = displacementTrainingData, dataToBeNnormalized = displacementValidationData, typeOfDataSubset = 'Displacement Validation')[0]
-    displacementTestData = normalize_data(dataUsedToNormalize = displacementTrainingData, dataToBeNnormalized = displacementTestData, typeOfDataSubset = 'Displacement Test')[0]
-    displacementTrainingData, normalizingDisplacementParameters = normalize_data(dataUsedToNormalize = displacementTrainingData, dataToBeNnormalized = displacementTrainingData, typeOfDataSubset = 'Displacement Training')
+    displacement_validation_data = normalize_data(data_used_to_normalize = displacement_training_data, data_to_be_normalized = displacement_validation_data, type_of_data_subset = 'Displacement Validation')[0]
+    displacement_test_data = normalize_data(data_used_to_normalize = displacement_training_data, data_to_be_normalized = displacement_test_data, type_of_data_subset = 'Displacement Test')[0]
+    displacement_training_data, normalizing_displacement_parameters = normalize_data(data_used_to_normalize = displacement_training_data, data_to_be_normalized = displacement_training_data, type_of_data_subset = 'Displacement Training')
 
-    unnormalizedForceTraining = forceTrainingData
+    unnormalized_force_training = force_training_data
 
     # Normalize the force training datasets
-    forceValidationData = normalize_data(dataUsedToNormalize = forceTrainingData, dataToBeNnormalized = forceValidationData, typeOfDataSubset = 'Force Validation')[0]
-    forceTestData = normalize_data(dataUsedToNormalize = forceTrainingData, dataToBeNnormalized = forceTestData, typeOfDataSubset = 'Force Test')[0]
-    forceTrainingData, normalizingDisplacementParameters = normalize_data(dataUsedToNormalize = forceTrainingData, dataToBeNnormalized = forceTrainingData, typeOfDataSubset = 'Force Training')
+    force_validation_data = normalize_data(data_used_to_normalize = force_training_data, data_to_be_normalized = force_validation_data, type_of_data_subset = 'Force Validation')[0]
+    force_test_data = normalize_data(data_used_to_normalize = force_training_data, data_to_be_normalized = force_test_data, type_of_data_subset = 'Force Test')[0]
+    force_training_data, normalizing_displacement_parameters = normalize_data(data_used_to_normalize = force_training_data, data_to_be_normalized = force_training_data, type_of_data_subset = 'Force Training')
 
     epochs = 1
-    model = cnn(displacementTrainingData.shape[1:]).build_model()
+    model = CNN(displacement_training_data.shape[1:]).build_model()
     model.compile(optimizer=optimizers.Adamax(learning_rate=0.001), loss='mae', metrics='mse')
 
     model.summary()
     stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
-    history = model.fit(displacementTrainingData, forceTrainingData, epochs=epochs, batch_size=16, callbacks=[stop_early], validation_data=(displacementValidationData, forceValidationData))
+    history = model.fit(displacement_training_data, force_training_data, epochs=epochs, batch_size=16, callbacks=[stop_early], validation_data=(displacement_validation_data, force_validation_data))
 
     # Save the trained model
-    model.save(fileDirectoryToSaveResults + '/trainedModel.h5')
-    print('Saved trainedModel.h5 to %s.' %fileDirectoryToSaveResults)
+    model.save(file_directory_to_save_results + '/trainedModel.h5')
+    print('Saved trainedModel.h5 to %s.' %file_directory_to_save_results)
     
-    plot = plots(history, fileDirectoryToSaveResults)
-    lossPlot = plot.loss()
-    evaluationMetricPlot = plot.evaluation_metric()
+    plot = plots(history, file_directory_to_save_results)
+    loss_plot = plot.loss()
+    evaluation_metric_plot = plot.evaluation_metric()
 
 if __name__ == '__main__':
 
